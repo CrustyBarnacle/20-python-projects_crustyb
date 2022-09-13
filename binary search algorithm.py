e43aa916e262cd380f241d759140bdf76d723744
Mario Burgos <CrustyBarnacle@users.noreply.github.com>
2022-09-12 22:24:41 -0700

    Updates binary_search.py
    Minor refactor of original. Still only steps through process of reducing size of list, but will not always return index of target.

--- binary search algorithm.py
+++ binary search algorithm.py
@@ -1,25 +1,51 @@
-def binary_search(list, element):
-    middle = 0
-    start = 0
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Mario Burgos
+# Tutorial: https://github.com/tomitokko/20-python-projects
+# Youtube: https://youtu.be/pdy3nh1tn6I
+# Created Date: Mon Sep 12 2022
+# version ='1.0'
+
+
+# Get list (to search) and target to look for
+# Calculate (of list): middle, start, end, steps (taken so far)
+# Recurse/loop through list, increment step count
+#   Each loop: 
+# Once found, return element/value
+
+
+def binary_search(list, target):
+    start = steps = 0
     end = len(list)
-    steps = 0
-    
-    while(start<=end):
-        print("Step", steps, ":" ,str(list[start:end+1]))
-        steps = steps+1
+
+    while(start < end):
         middle = (start + end) // 2
-        
-        if element == list[middle] :
+        print(f'Step: {steps} : {list[start:end+1]}')
+
+        steps +=1
+
+        if target == list[middle]:
             return middle
-        if element < list[middle]:
-            end = middle -1
+        elif target < list[middle]:
+            end = middle - 1
         else:
             start = middle + 1
 
     return -1
 
 
-my_list = [1,2,3,4,5,6,7,8,9,10,11,12]
-target = 12
+def get_input():
+    list = input("List to use: ")
+    list = list.split()
+    target = input("Search target: ")
 
-binary_search(my_list, target)
+    return list, target
+
+
+def main():
+    list, target = get_input()
+    binary_search(list, target)
+
+
+main()
