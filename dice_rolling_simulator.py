122e8a5961e9fd9565d893191425da9933893c45
Mario Burgos <CrustyBarnacle@users.noreply.github.com>
2022-09-21 09:59:37 -0700

    Update dice_rolling_simulator.py
    Minimal changes to "dice" face ASCII text. Adds placeholder for v2.0 for breaking out into functions and allow for different sided dice to be used (d4, d8, d12, d20, dn).

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python3
+
+""" -*- coding: utf-8 -*-
+ Created By  : Mario Burgos
+ Tutorial: https://github.com/tomitokko/20-python-projects
+ Youtube: https://youtu.be/pdy3nh1tn6I
+ Created Date: Tue Sep 20 2022
+ version ='1.0'
+"""
+
+""" Placeholder for v2.0
+import random
+
+
+def roll_dice(sides):
+    print(f'd{sides}: ')
+    return random.randint(1,sides)
+
+
+def main():
+    print(f'Random result: {roll_dice()}')
+
+
+main()
+
+"""
+
 import random
 
 def roll_dice():
@@ -5,49 +32,51 @@ def roll_dice():
     dice_drawing = {
         1: (
             "-----",
-            "|   |",
-            "| o |",
-            "|   |",
-            "-----",
+            "|     |",
+            "|  o  |",
+            "|     |",
+            "-------",
         ),
         2: (
             "-----",
-            "|o  |",
-            "|   |",
-            "|  o|",
-            "-----",
+            "| o   |",
+            "|     |",
+            "|   o |",
+            "-------",
         ),
         3: (
-            "-----",
-            "|o  |",
-            "| o |",
-            "|  o|",
-            "-----",
+            "-------",
+            "| o   |",
+            "|  o  |",
+            "|   o |",
+            "-------",
         ),
         4: (
-            "-----",
-            "|o o|",
-            "|   |",
-            "|o o|",
-            "-----",
+            "-------",
+            "| o o |",
+            "|     |",
+            "| o o |",
+            "-------",
         ),
         5: (
-            "-----",
-            "|o o|",
-            "| o |",
-            "|o o|",
-            "-----",
+            "-------",
+            "| o o |",
+            "|  o  |",
+            "| o o |",
+            "-------",
         ),
         6: (
-            "-----",
-            "|o o|",
-            "|o o|",
-            "|o o|",
-            "-----",
+            "-------",
+            "| o o |",
+            "| o o |",
+            "| o o |",
+            "-------",
         ),
 
     }
+
     roll = input("Roll the dice? (Yes/No) : ")
+
     while roll.lower() == "Yes". lower():
         dice1 = random.randint(1, 6)
         dice2 = random.randint(1, 6)
@@ -57,4 +86,6 @@ def roll_dice():
         print("\n".join(dice_drawing[dice2]))
         
         roll = input("Roll again? (Yes/no): ")
+
+
 roll_dice()
